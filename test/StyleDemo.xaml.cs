75b4fc50fe79d0e5964efd75d18b6229b23dd7e5
ZNATZ <Z.NAT.CN@GMAIL.COM>
2012-11-13 00:16:34 +0900

    Next A Taste of data binding.

--- test/StyleDemo.xaml.cs
+++ test/StyleDemo.xaml.cs
@@ -23,6 +23,18 @@ namespace test
     {
         public StyleDemo()
         {
+            Style style2 = new Style(typeof(TextBlock));
+            style2.Setters.Add(new Setter(TextBlock.FontFamilyProperty,new FontFamily("Chiller")));
+            style2.Setters.Add(new Setter(TextBlock.FontSizeProperty, 50));
+            style2.Setters.Add(new Setter(TextBlock.VerticalAlignmentProperty, VerticalAlignment.Bottom));
+            style2.Setters.Add(new Setter(TextBlock.HorizontalAlignmentProperty, HorizontalAlignment.Center));
+
+            Style fromStyle2 = new Style(typeof(TextBlock));
+            fromStyle2.BasedOn = style2;
+            fromStyle2.Setters.Add(new Setter(TextBlock.MarginProperty,"0 0 0 100"));
+
+            this.Resources.Add("MyTxtStyle2", style2);
+            this.Resources.Add("FromMyTxtStyle2", fromStyle2);
             this.InitializeComponent();
         }
 
@@ -33,12 +45,31 @@ namespace test
         /// property is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            // Style created in XAML
             TextBlock txt = new TextBlock
             {
                 Style = App.Current.Resources["MyTxtStyle"] as Style,
-                Text = "Resources Applied"
+                Text = "XAML style resources applied.",
+                FontFamily  = new FontFamily("Lucida Calligraphy")  // Notice this line override the style in MyTxtStyle.
             };
+
+            // Style created in C#
+            TextBlock txt2 = new TextBlock
+            {
+                Style = this.Resources["MyTxtStyle2"] as Style,
+                Text = "Code generated style resources applied."
+            };
+
+            // Style created in C# derived from another style
+            TextBlock txt3 = new TextBlock
+            {
+                Style = this.Resources["FromMyTxtStyle2"] as Style,
+                Text = "Code generated derived style resources applied."
+            };
+
             (this.Content as Grid).Children.Add(txt);
+            (this.Content as Grid).Children.Add(txt2);
+            (this.Content as Grid).Children.Add(txt3);
         }
     }
 }
